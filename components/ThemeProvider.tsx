27bcc93df2b60f33dc3ff65abb72ffda7ff71734
Umut Erdoğan <109750600+umuterdgn@users.noreply.github.com>
2026-08-27 18:29:00 +0300

    fix: next-themes type import path resolved

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -6,8 +6,7 @@
 
 "use client"
 
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider attribute="class" defaultTheme="system" enableSystem {...props}>{children}</NextThemesProvider>
