fb3f1ff99ecfec7df16a2afb7a2df5bcddbfbc11
Umut Erdoğan <109750600+umuterdgn@users.noreply.github.com>
2026-08-27 19:38:49 +0300

    fix: removed invalid skipWaiting property from next-pwa config to pass type check

--- next.config.ts
+++ next.config.ts
@@ -65,7 +65,6 @@ export default withPWA({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   register: true,
-  skipWaiting: true,
   workboxOptions: {
     disableDevLogs: true,
   },
